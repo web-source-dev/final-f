42395e15e122458e60801c5a7b108bcebe07ba3a
web-source-dev <muhammadnouman72321@gmail.com>
2024-11-20 13:55:21 +0500

    Update ViewData.jsx

--- src/Components/ViewData.jsx
+++ src/Components/ViewData.jsx
@@ -150,6 +150,14 @@ const ViewData = () => {
    if (!isAuthenticated && twoFAEnabled) {
     return (
       <div className="two-fa-container">
+          <div className="logo-of-harmony-4all">
+    <img
+      src="https://static.wixstatic.com/media/e65032_cd33c8b9dc8d4a4b986f7fa5ac06df3e~mv2.jpg/v1/crop/x_337,y_634,w_1319,h_753/fill/w_133,h_76,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Harmony%204%20All%20logo_G2%20(2).jpg"
+      alt="Harmony 4All"
+      className="logo-image"
+    />
+  </div>
+
         <h1>Two-Factor Authentication</h1>
         <p>Please answer the security question to continue:</p>
         <p><strong>Which city is known as the "Big Apple"?</strong></p>
